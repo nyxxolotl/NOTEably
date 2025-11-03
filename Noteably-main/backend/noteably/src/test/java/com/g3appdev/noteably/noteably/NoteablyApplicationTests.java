2b86934e866c32a3b1abe45d0589a9bc48f267ce
nyxxolotl <maryannemalinao18@gmail.com>
2025-11-03 13:38:09 +0800

    FIXED Set up

--- Noteably-main/backend/noteably/src/test/java/com/g3appdev/noteably/noteably/NoteablyApplicationTests.java
+++ Noteably-main/backend/noteably/src/test/java/com/g3appdev/noteably/noteably/NoteablyApplicationTests.java
@@ -3,13 +3,19 @@ package com.g3appdev.noteably;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
 import com.g3appdev.noteably.NoteablyApplication;
 import com.g3appdev.noteably.config.SecurityConfig;
 
 @SpringBootTest(classes = NoteablyApplication.class)
 @Import(SecurityConfig.class)
 class NoteablyApplicationTests {
+
+	@MockBean
+    private com.g3appdev.noteably.Service.StudentService studentService;
+
+    @MockBean
+    private com.g3appdev.noteably.config.JWTAuthFilter jwtAuthFilter;
 
 	@Test
 	void contextLoads() {
